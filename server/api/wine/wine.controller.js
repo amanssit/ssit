751f6437a53db1c746037fed4a4b97408b42f10a
Manjesh V <manjeshpv@gmail.com>
2015-06-05 00:12:41 +0530

    Created all controllers with blank array response

--- server/api/wine/wine.controller.js
+++ server/api/wine/wine.controller.js
@@ -2,7 +2,33 @@
 
 var _ = require('lodash');
 
+
 // Get list of wines
-exports.index = function (req, res) {
+exports.Index = function (req, res) {
+  res.json([]);
+};
+
+// Search wines
+exports.Search = function (req, res) {
+  res.json([]);
+};
+
+// Get a  wine by ID
+exports.GetByID = function (req, res) {
+  res.json([]);
+};
+
+// Add a wine
+exports.Add = function (req, res) {
+  res.json([]);
+};
+
+// Update wine based on ID
+exports.Update = function (req, res) {
+  res.json([]);
+};
+
+// Delete Wine Based on ID
+exports.Delete = function (req, res) {
   res.json([]);
 };
